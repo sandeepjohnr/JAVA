e51301be62c83213252b81497a26dfb7333aa458
sandeepjohnr <sandeepjohnrofficial@gmail.com>
2026-02-12 19:50:16 +0530

    day2

---
+++ day_2/jspiders.java
@@ -0,0 +1,10 @@
+class jspiders{
+public static void createOTP(){
+System.out.println("otp created");
+}
+public static void main(String[]agrs){
+System.out.println("Starts");
+createOTP();
+System.out.println("Ends");
+}
+}
